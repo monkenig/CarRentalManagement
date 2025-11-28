713a85bfb7600ef1f7b5fc76b133cadfc45818f7
joeden <joedenlem@gmail.com>
2025-11-28 15:02:15 +0800

    Added database and seed data

---
+++ CarRentalManagement/Configurations/Entities/ModelSeed.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using CarRentalManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace CarRentalManagement.Configurations.Entities
+{
+    public class ModelSeed : IEntityTypeConfiguration<Model>
+    {
+        public void Configure(EntityTypeBuilder<Model> builder)
+        {
+            builder.HasData(
+                new Model
+                {
+                    Id = 1,
+                    Name = "i4",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Model
+                {
+                    Id = 2,
+                    Name = "X5",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Model
+                {
+                    Id = 3,
+                    Name = "Prius",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                },
+                new Model
+                {
+                    Id = 4,
+                    Name = "C-HR",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+            );
+        }
+    }
+}
